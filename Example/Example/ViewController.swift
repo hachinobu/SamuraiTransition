935e2aa8305c9fed915fd4efa27ae4650e34e816
hachinobu <hachinobu@gmail.com>
2016-11-26 14:40:42 +0900

    created a project file

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Takahiro Nishinobu on 2016/11/26.
+//  Copyright © 2016年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SamuraiTransition/SamuraiTransition.h
@@ -0,0 +1,19 @@
+//
+//  SamuraiTransition.h
+//  SamuraiTransition
+//
+//  Created by Takahiro Nishinobu on 2016/11/26.
+//  Copyright © 2016年 hachinobu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SamuraiTransition.
+FOUNDATION_EXPORT double SamuraiTransitionVersionNumber;
+
+//! Project version string for SamuraiTransition.
+FOUNDATION_EXPORT const unsigned char SamuraiTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamuraiTransition/PublicHeader.h>
+
+
