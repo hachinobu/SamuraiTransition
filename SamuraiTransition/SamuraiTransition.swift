f79186c09ad2eb52af489002fe6ab7ff92e8bde1
hachinobu <hachinobu@gmail.com>
2016-11-27 21:16:12 +0900

    refactor common processing

--- SamuraiTransition/SamuraiTransition.swift
+++ SamuraiTransition/SamuraiTransition.swift
@@ -37,6 +37,12 @@ public class SamuraiTransition: NSObject {
         return fromViewController.view
     }
     
+    fileprivate let coverView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
 }
 
 extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
@@ -50,53 +56,44 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
         self.transitionContext = transitionContext
         containerView = transitionContext.containerView
         
+        let zanTargetView = presenting ? fromView.snapshotView(afterScreenUpdates: false)! : toView.snapshotView(afterScreenUpdates: true)!
+        let zanPoint = containerView.center
+        
+        let oneSideOffsetFrame: CGRect
+        let otherSideOffsetFrame: CGRect
+        let divided: (slice: CGRect, remainder: CGRect)
+        
+        if zanAngle.isHorizontal() {
+            divided = containerFrame.divided(atDistance: zanPoint.y, from: .minYEdge)
+            oneSideOffsetFrame = divided.slice.offsetBy(dx: 0.0, dy: -divided.slice.height)
+            otherSideOffsetFrame = divided.remainder.offsetBy(dx: 0.0, dy: divided.remainder.height)
+        } else {
+            divided = containerFrame.divided(atDistance: zanPoint.x, from: .minXEdge)
+            oneSideOffsetFrame = divided.slice.offsetBy(dx: -divided.slice.width, dy: 0.0)
+            otherSideOffsetFrame = divided.remainder.offsetBy(dx: divided.remainder.width, dy: 0.0)
+        }
+        
+        let oneSideView = zanTargetView.resizableSnapshotView(from: divided.slice, afterScreenUpdates: false, withCapInsets: .zero)!
+        let otherSideView = zanTargetView.resizableSnapshotView(from: divided.remainder, afterScreenUpdates: false, withCapInsets: .zero)!
+        
+        containerView.addSubview(oneSideView)
+        containerView.addSubview(otherSideView)
+        
         if presenting {
             
-            let zanTargetView = fromView.snapshotView(afterScreenUpdates: false)!
-            let zanPoint = containerView.center
-            let oneSideSnapFrame: CGRect
-            let otherSideSnapFrame: CGRect
-            
-            let oneSideFinishFrame: CGRect
-            let otherSideFnishFrame: CGRect
-            
-            if zanAngle.isHorizontal() {
-                let divided = containerFrame.divided(atDistance: zanPoint.y, from: .minYEdge)
-                oneSideSnapFrame = divided.slice
-                otherSideSnapFrame = divided.remainder
-                oneSideFinishFrame = oneSideSnapFrame.offsetBy(dx: 0.0, dy: -oneSideSnapFrame.height)
-                otherSideFnishFrame = otherSideSnapFrame.offsetBy(dx: 0.0, dy: otherSideSnapFrame.height)
-            } else {
-                let divided = containerFrame.divided(atDistance: zanPoint.x, from: .minXEdge)
-                oneSideSnapFrame = divided.slice
-                otherSideSnapFrame = divided.remainder
-                oneSideFinishFrame = oneSideSnapFrame.offsetBy(dx: -oneSideSnapFrame.width, dy: 0.0)
-                otherSideFnishFrame = otherSideSnapFrame.offsetBy(dx: otherSideSnapFrame.width, dy: 0.0)
-            }
-            
-            let oneSideView = zanTargetView.resizableSnapshotView(from: oneSideSnapFrame, afterScreenUpdates: false, withCapInsets: .zero)!
-            oneSideView.frame = oneSideSnapFrame
-            
-            let otherSideView = zanTargetView.resizableSnapshotView(from: otherSideSnapFrame, afterScreenUpdates: false, withCapInsets: .zero)!
-            otherSideView.frame = otherSideSnapFrame
-            
-            containerView.addSubview(oneSideView)
-            containerView.addSubview(otherSideView)
+            oneSideView.frame = divided.slice
+            otherSideView.frame = divided.remainder
             containerView.insertSubview(toView, belowSubview: oneSideView)
-            toView.alpha = 0.3
+            
+            toView.alpha = 0.0
             toView.transform = CGAffineTransform(scaleX: 0.4, y: 0.3)
             
-            let blackView = UIView(frame: containerFrame)
-            blackView.backgroundColor = .black
-            containerView.insertSubview(blackView, belowSubview: toView)
+            coverView.frame = containerFrame
+            containerView.insertSubview(coverView, belowSubview: toView)
             
-//            let oneSideViewAnimation = CABasicAnimation(keyPath: "position")
-//            oneSideViewAnimation.duration = duration
-//            oneSideViewAnimation.toValue = NSValue(cgPoint: oneSideFinishFrame.origin)
-            
-            UIView.animate(withDuration: duration, animations: { 
-                oneSideView.frame = oneSideFinishFrame
-                otherSideView.frame = otherSideFnishFrame
+            UIView.animate(withDuration: duration, animations: {
+                oneSideView.frame = oneSideOffsetFrame
+                otherSideView.frame = otherSideOffsetFrame
                 self.toView.alpha = 1.0
                 self.toView.transform = CGAffineTransform.identity
             }, completion: { _ in
@@ -107,45 +104,16 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
             
         } else {
             
-            let zanTargetView = toView.snapshotView(afterScreenUpdates: true)!
-            let zanPoint = containerView.center
+            oneSideView.frame = oneSideOffsetFrame
+            otherSideView.frame = otherSideOffsetFrame
+            containerView.insertSubview(fromView, belowSubview: oneSideView)
             
-            let oneSideSnapFrame: CGRect
-            let otherSideSnapFrame: CGRect
-            
-            let oneSideFinishFrame: CGRect
-            let otherSideFnishFrame: CGRect
-            
-            if zanAngle.isHorizontal() {
-                let divided = containerFrame.divided(atDistance: zanPoint.y, from: .minYEdge)
-                oneSideSnapFrame = divided.slice
-                otherSideSnapFrame = divided.remainder
-                oneSideFinishFrame = oneSideSnapFrame.offsetBy(dx: 0.0, dy: -oneSideSnapFrame.height)
-                otherSideFnishFrame = otherSideSnapFrame.offsetBy(dx: 0.0, dy: otherSideSnapFrame.height)
-            } else {
-                let divided = containerFrame.divided(atDistance: zanPoint.x, from: .minXEdge)
-                oneSideSnapFrame = divided.slice
-                otherSideSnapFrame = divided.remainder
-                oneSideFinishFrame = oneSideSnapFrame.offsetBy(dx: -oneSideSnapFrame.width, dy: 0.0)
-                otherSideFnishFrame = otherSideSnapFrame.offsetBy(dx: otherSideSnapFrame.width, dy: 0.0)
-            }
-            
-            let oneSideView = zanTargetView.resizableSnapshotView(from: oneSideSnapFrame, afterScreenUpdates: false, withCapInsets: .zero)!
-            oneSideView.frame = oneSideFinishFrame
-            
-            let otherSideView = zanTargetView.resizableSnapshotView(from: otherSideSnapFrame, afterScreenUpdates: false, withCapInsets: .zero)!
-            otherSideView.frame = otherSideFnishFrame
-            
-            containerView.addSubview(fromView)
-            containerView.addSubview(oneSideView)
-            containerView.addSubview(otherSideView)
             toView.alpha = 0.0
-            
             UIView.animate(withDuration: duration, animations: {
                 self.fromView.alpha = 0.0
                 self.fromView.transform = CGAffineTransform(scaleX: 0.4, y: 0.3)
-                oneSideView.frame = oneSideSnapFrame
-                otherSideView.frame = otherSideSnapFrame
+                oneSideView.frame = divided.slice
+                otherSideView.frame = divided.remainder
             }, completion: { _ in
                 self.toView.alpha = 1.0
                 self.fromView.removeFromSuperview()
