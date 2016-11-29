1df1e7b94f20fabc85e2473f6c92b1bc944bcb40
hachinobu <hachinobu@gmail.com>
2016-11-30 00:31:23 +0900

    Changed ZanLine from UIView to Layer

--- SamuraiTransition/SamuraiTransition.swift
+++ SamuraiTransition/SamuraiTransition.swift
@@ -16,7 +16,7 @@ public class SamuraiTransition: NSObject {
     public var isAffineTransform: Bool = true
     public var zanPosition: CGPoint?
     public var zanLineColor = UIColor.black
-    public var zanLineWidth: CGFloat = 1.0
+    public var zanLineWidth: CGFloat = 0.3
     
     fileprivate weak var transitionContext: UIViewControllerContextTransitioning!
     fileprivate var containerView: UIView!
@@ -75,43 +75,45 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
         let otherSideOffsetFrame: CGRect
         let slice: CGRect
         let remainder: CGRect
-        let zanLine: UIView
-        let zanEndPoint: CGSize
         
         var oneSideMaskLayer: CAShapeLayer? = nil
         var otherSideMaskLayer: CAShapeLayer? = nil
+        let lineLayer: CAShapeLayer
         
         if zanAngle.isHorizontal() {
+            
             let divided = containerFrame.divided(atDistance: zanPoint.y, from: .minYEdge)
             slice = divided.slice
             remainder = divided.remainder
             oneSideOffsetFrame = slice.offsetBy(dx: 0.0, dy: -slice.height)
             otherSideOffsetFrame = remainder.offsetBy(dx: 0.0, dy: remainder.height)
             
-            zanLine = UIView(frame: CGRect(x: 0.0, y: zanPoint.y, width: 0.0, height: zanLineWidth))
-            zanEndPoint = CGSize(width: containerFrame.maxX, height: zanLine.frame.height)
+            lineLayer = zanLineLayer(from: CGPoint(x: containerFrame.minX, y: zanPoint.y), end: CGPoint(x: containerFrame.maxX, y: zanPoint.y))
             
         } else if zanAngle.isVertical() {
+            
             let divided = containerFrame.divided(atDistance: zanPoint.x, from: .minXEdge)
             slice = divided.slice
             remainder = divided.remainder
             oneSideOffsetFrame = slice.offsetBy(dx: -slice.width, dy: 0.0)
             otherSideOffsetFrame = remainder.offsetBy(dx: remainder.width, dy: 0.0)
             
-            zanLine = UIView(frame: CGRect(x: zanPoint.x, y: 0.0, width: zanLineWidth, height: 0.0))
-            zanEndPoint = CGSize(width: zanLine.frame.width, height: containerFrame.maxY)
+            lineLayer = zanLineLayer(from: CGPoint(x: zanPoint.x, y: containerFrame.minY), end: CGPoint(x: zanPoint.x, y: containerFrame.maxY))
+            
         } else {
+            
             slice = containerFrame
             remainder = containerFrame
             oneSideOffsetFrame = slice.offsetBy(dx: slice.width, dy: slice.height)
             otherSideOffsetFrame = remainder.offsetBy(dx: -remainder.width, dy: -remainder.height)
             
-            zanLine = UIView(frame: CGRect(x: containerFrame.maxX, y: 0.0, width: 1.0, height: 1.0))
-            zanEndPoint = .zero
-            
             let maskLayers = maskLayer(zanPosition: zanPoint)
             oneSideMaskLayer = maskLayers.oneSide
             otherSideMaskLayer = maskLayers.otherSide
+            
+            let bottomX = diagnoallyBottomX(zanPosition: zanPoint)
+            lineLayer = zanLineLayer(from: CGPoint(x: containerFrame.maxX, y: containerFrame.minY), end: CGPoint(x: bottomX, y: containerFrame.maxY))
+            
         }
         
         let oneSideView = zanTargetView.resizableSnapshotView(from: slice, afterScreenUpdates: false, withCapInsets: .zero)!
@@ -136,25 +138,22 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
             coverView.frame = containerFrame
             containerView.insertSubview(coverView, belowSubview: toView)
             
-            zanLine.backgroundColor = zanLineColor
-            containerView.addSubview(zanLine)
+            containerView.layer.addSublayer(lineLayer)
+            let animation = lineLayerAnimation(lineLayer: lineLayer)
+            lineLayer.add(animation, forKey: nil)
             
-            UIView.animate(withDuration: zanLineDuration, animations: {
-                zanLine.frame.size = zanEndPoint
+            UIView.animate(withDuration: duration - zanLineDuration, delay: zanLineDuration, animations: {
+                
+                oneSideView.frame = oneSideOffsetFrame
+                otherSideView.frame = otherSideOffsetFrame
+                self.toView.alpha = 1.0
+                self.toView.transform = CGAffineTransform.identity
+                
             }, completion: { _ in
                 
-                zanLine.removeFromSuperview()
-                
-                UIView.animate(withDuration: self.duration - self.zanLineDuration, animations: {
-                    oneSideView.frame = oneSideOffsetFrame
-                    otherSideView.frame = otherSideOffsetFrame
-                    self.toView.alpha = 1.0
-                    self.toView.transform = CGAffineTransform.identity
-                }, completion: { _ in
-                    oneSideView.removeFromSuperview()
-                    otherSideView.removeFromSuperview()
-                    transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-                })
+                oneSideView.removeFromSuperview()
+                otherSideView.removeFromSuperview()
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
                 
             })
             
@@ -182,11 +181,40 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
         
     }
     
-    private func maskLayer(zanPosition: CGPoint) -> (oneSide: CAShapeLayer, otherSide: CAShapeLayer) {
+}
+
+extension SamuraiTransition {
+    
+    fileprivate func lineLayerAnimation(lineLayer: CAShapeLayer) -> CABasicAnimation {
         
-        let oneSidePath = UIBezierPath()
-        oneSidePath.move(to: CGPoint(x: containerFrame.maxX, y: containerFrame.minY))
-        oneSidePath.addLine(to: zanPosition)
+        let lineAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        lineAnimation.duration = zanLineDuration
+        lineAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        lineAnimation.fromValue = 0.0
+        lineAnimation.toValue = 1.0
+        lineAnimation.delegate = self
+        lineAnimation.setValue(lineLayer, forKey: "LineLayer")
+        
+        return lineAnimation
+    }
+    
+    fileprivate func zanLineLayer(from start: CGPoint, end: CGPoint) -> CAShapeLayer {
+        
+        let zanLineLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addLine(to: end)
+        path.close()
+        zanLineLayer.path = path.cgPath
+        zanLineLayer.fillColor = nil
+        zanLineLayer.strokeColor = zanLineColor.cgColor
+        zanLineLayer.lineWidth = zanLineWidth
+        return zanLineLayer
+        
+    }
+    
+    fileprivate func diagnoallyBottomX(zanPosition: CGPoint) -> CGFloat {
+        
         let ratio = containerFrame.maxY / zanPosition.y
         if ratio.isInfinite {
             fatalError("zanPosition.y is not 0. It will be infinite.")
@@ -194,13 +222,36 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
         let width = (containerFrame.maxX - zanPosition.x) * ratio
         let bottomX = containerFrame.maxX - width
         
+        return bottomX
+    }
+    
+    fileprivate func maskLayer(zanPosition: CGPoint) -> (oneSide: CAShapeLayer, otherSide: CAShapeLayer) {
+        let bottomX = diagnoallyBottomX(zanPosition: zanPosition)
+        
+        let oneSidePath = oneSideBezierPath(zanPosition: zanPosition, bottomX: bottomX)
+        let oneSideLayer = CAShapeLayer()
+        oneSideLayer.path = oneSidePath.cgPath
+        
+        let otherSidePath = otherSideBezierPath(zanPosition: zanPosition, bottomX: bottomX)
+        let otherSideLayer = CAShapeLayer()
+        otherSideLayer.path = otherSidePath.cgPath
+        
+        return (oneSideLayer, otherSideLayer)
+    }
+    
+    private func oneSideBezierPath(zanPosition: CGPoint, bottomX: CGFloat) -> UIBezierPath {
+        let oneSidePath = UIBezierPath()
+        oneSidePath.move(to: CGPoint(x: containerFrame.maxX, y: containerFrame.minY))
+        oneSidePath.addLine(to: zanPosition)
         oneSidePath.addLine(to: CGPoint(x: bottomX, y: containerFrame.maxY))
         oneSidePath.addLine(to: CGPoint(x: containerFrame.maxX, y: containerFrame.maxY))
         oneSidePath.addLine(to: CGPoint(x: containerFrame.maxX, y: containerFrame.minY))
         oneSidePath.close()
-        let oneSideLayer = CAShapeLayer()
-        oneSideLayer.path = oneSidePath.cgPath
         
+        return oneSidePath
+    }
+    
+    private func otherSideBezierPath(zanPosition: CGPoint, bottomX: CGFloat) -> UIBezierPath {
         let otherSidePath = UIBezierPath()
         otherSidePath.move(to: CGPoint(x: containerFrame.minX, y: containerFrame.minY))
         otherSidePath.addLine(to: CGPoint(x: containerFrame.maxX, y: containerFrame.minY))
@@ -209,10 +260,18 @@ extension SamuraiTransition: UIViewControllerAnimatedTransitioning {
         otherSidePath.addLine(to: CGPoint(x: containerFrame.minX, y: containerFrame.minY))
         otherSidePath.close()
         
-        let otherSideLayer = CAShapeLayer()
-        otherSideLayer.path = otherSidePath.cgPath
-        
-        return (oneSideLayer, otherSideLayer)
+        return otherSidePath
     }
     
 }
+
+extension SamuraiTransition: CAAnimationDelegate {
+    
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if let layer = anim.value(forKey: "LineLayer") as? CAShapeLayer {
+            layer.removeFromSuperlayer()
+        }
+    }
+    
+}
+
