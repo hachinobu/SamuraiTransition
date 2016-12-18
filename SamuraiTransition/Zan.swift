bf4744c19cc016a6a3b2267dcabf2ac427a19641
hachinobu <hachinobu@gmail.com>
2016-12-18 22:47:38 +0900

    Add rectangle zan animation

--- SamuraiTransition/Zan.swift
+++ SamuraiTransition/Zan.swift
@@ -17,6 +17,7 @@ public enum Zan {
     case x
     case jagged(width: CGFloat)
     case circle(radius: CGFloat)
+    case rectangle(width: CGFloat, height: CGFloat, cornerRadius: CGFloat)
     
     func samuraiConfig(containerFrame: CGRect, zanPoint: CGPoint, lineWidth: CGFloat, color: UIColor) -> SamuraiConfigProtocol {
         
@@ -41,7 +42,9 @@ public enum Zan {
             
         case let .circle(radius):
             return CircleZanConfig(containerFrame: containerFrame, zanPoint: zanPoint, width: lineWidth, color: color, radius: radius)
-        
+            
+        case let .rectangle(width, height, cornerRadius):
+            return RectangleZanConfig(containerFrame: containerFrame, zanPoint: zanPoint, lineWidth: lineWidth, lineColor: color, rectangleWidth: width, rectangleHeight: height, cornerRadius: cornerRadius)
         }
         
     }
